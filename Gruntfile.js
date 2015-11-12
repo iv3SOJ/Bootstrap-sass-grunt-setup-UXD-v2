f777909ae9965dabe2bd915f2d0050b7caf9b7b3
jarvinmito <amito@yondu.com>
2015-11-12 16:06:20 +0800

    update setup
    - modified config.json
    - updated gruntfile setup
    - updated package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,149 +1,216 @@
 module.exports = function (grunt) {
-  // Load Grunt tasks declared in the package.json file
+
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
+  // Setup
   grunt.initConfig({
-    // Get information from config.json regarding the location of the project
-    pkg: grunt.file.readJSON('config.json'),
-    
-    rev: {
+    mods : {
+      dev : ['grunt-sass','grunt-contrib-watch','grunt-express','grunt-open','grunt-rev']
+    },
+    pkg: grunt.file.readJSON('config.json')
+  });
+
+
+  grunt.config('env', grunt.option('env') || process.env.GRUNT_ENV || 'dev');
+  grunt.config('compress', grunt.config('env') === 'prod');
+
+  // Disable caching of files
+  grunt.config('rev', {
+    options: {
+      encoding: 'utf8',
+      algorithm: 'md5',
+      length: 5
+    },
+    assets: {
+      files: [{
+        src: [
+          'js/*.js',
+          'css/*.css'
+        ]
+      }]
+    }
+  });
+
+  // SASS compilation
+  grunt.config('sass', {
+    dev : {
       options: {
-        encoding: 'utf8',
-        algorithm: 'md5',
-        length: 5
+        includePaths: ['bower_components/bootstrap-sass/assets/stylesheets'],
+        outputStyle: 'expanded',
+        compress : grunt.config('compress')
       },
-      assets: {
-        files: [{
-          src: [
-						'js/*.js',
-						'css/*.css'
-					]
-				}]
+      files: {
+        'css/app.css': 'scss/app.scss'
       }
     },
-    // copy files
-    copy: {
-      main: {
-        files: [
-                    // Vendor scripts.
-          {
-            expand: true,
-            cwd: 'bower_components/bootstrap-sass/assets/javascripts/',
-            src: ['**/*.js'],
-            dest: 'scripts/bootstrap-sass/'
-                    },
-          {
-            expand: true,
-            cwd: 'bower_components/jquery/dist/',
-            src: ['**/*.js', '**/*.map'],
-            dest: 'scripts/jquery/'
-                    },
-
-                    // Fonts.
-          {
-            expand: true,
-            filter: 'isFile',
-            flatten: true,
-            cwd: 'bower_components/',
-            src: ['bootstrap-sass/assets/fonts/**'],
-            dest: 'fonts/'
-                    },
-
-                    // Stylesheets
-          {
-            expand: true,
-            cwd: 'bower_components/bootstrap-sass/assets/stylesheets/',
-            src: ['**/*.scss'],
-            dest: 'scss/'
-                    }
-                ]
+    dist : {
+      options : {
+        outputStyle : 'compressed'
       },
-    },
-
-    // SASS compilation
-    // Compile SASS files into minified CSS.
-    sass: {
-      dev: {
-        options: {
-          includePaths: [
-          'bower_components/bootstrap-sass/assets/stylesheets'
-        ],
-          style: 'expanded'
-        },
-        files: {
-          'css/app.css': 'scss/app.scss'
-        }
-      },
-
-      dist: {
-        options: {
-          outputStyle: 'compressed'
-        },
-        files: {
-          'dist/css/app.min.css': 'scss/app.scss'
-        }
-      }
-    },
-
-    // uglify
-    uglify: {
-      options: {
-        mangle: false
-      },
-      my_target: {
-        files: {
-          'js/app.js': ['scripts/*.js']
-        }
-      }
-    },
-
-    // The webserver
-    express: {
-      all: {
-        options: {
-          // Set your file directory 
-          bases : ['<%= pkg.location %>'],
-          port: 9000,
-          hostname: "0.0.0.0",
-          livereload: true
-        }
-      }
-    },
-
-    watch: {
-      all: {
-        files: ['Gruntfile.js', 'scripts/*.js', '*.html'],
-        tasks: ['uglify'],
-        options: {
-          livereload: true
-        }
-      },
-      css: {
-        files: [
-          'scss/*.scss',
-          'scss/**/*.scss',
-          'scss/**/**/*.scss'
-        ],
-        tasks: ['sass:dev','sass:dist']
-      }
-    },
-
-    open: {
-      all: {
-        path: 'http://localhost:9000/index.html',
-        app: 'Chrome'
+      files : {
+        'dist/css/app.css' : 'scss/app.scss'
       }
     }
   });
 
-  // grunt.loadNpmTasks('grunt-sass');
-  // grunt.loadNpmTasks('grunt-express');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
-  // grunt.loadNpmTasks('grunt-contrib-copy');
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-rev');
+  grunt.config('postcss', {
+    options: {
+      map: true, // inline sourcemaps
 
-  // Establish tasks we can run from the terminal.
-  grunt.registerTask('build', ['sass', 'copy', 'uglify', 'express', 'open']);
-  grunt.registerTask('default', ['build', 'watch']);
+      // or
+      map: {
+        inline: false, // save all sourcemaps as separate files...
+        annotation: 'css/maps/' // ...to the specified directory
+      },
+
+      processors: [
+      require('pixrem')(), // add fallbacks for rem units
+      require('autoprefixer')({
+          browsers: 'last 2 versions'
+        }) // add vendor prefixes
+    ]
+    },
+    dist: {
+      src: 'dist/css/*.css'
+    },
+    dev : {
+      src : 'css/*.css'
+    }
+  });
+
+  // Concat function for js files
+  grunt.config('concat', {
+    options : {
+      separator : ';'
+    },
+    dist: {
+        src : [
+          'bower_components/jquery/dist/jquery.min.js',
+          'bower_components/bootstrap-sass/assets/javascripts/{bootstrap.min,bootstrap-sprockets}.js',
+          'scripts/*.js'
+        ],
+        dest : 'dist/js/app.js',
+        nosort : false
+    },
+    dev: {
+        src : [
+          'scripts/*.js'
+        ],
+        dest : 'js/app.js',
+        nosort : false
+    }
+  });
+
+
+  grunt.config('injector', {
+    options: {},
+    my_target: {
+      files: {
+        '*.html': [
+          'bower_components/jquery/dist/jquery.js',
+          'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+          'js/app.js',
+          'css/app.css'
+        ]
+      }
+    }
+  });
+
+  // Uglify files
+  grunt.config('uglify', {
+    options: {
+      mangle: false
+    },
+    my_target: {
+      files: {
+        'dist/js/app.js': 'dist/js/app.js'
+      }
+    }
+  });
+
+  // HTML Minification
+  grunt.config('htmlmin',{
+    dist: {                                      // Target
+      options: {                                 // Target options
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      files: {                                   // Dictionary of files
+        'dist/index.html': 'index.html'
+      }
+    }
+  });
+
+  // Webserver setup
+  grunt.config('express', {
+    all: {
+      options: {
+        // Set your file directory 
+        bases : ['<%= pkg.location %>'],
+        port: 9000,
+        hostname: "0.0.0.0",
+        livereload: true
+      }
+    }
+  });
+
+  // Open Browser
+  grunt.config('open', {
+    all: {
+      path: 'http://localhost:9000/index.html',
+      app: 'Chrome'
+    }
+  });
+
+  // Watcher for file changes
+  grunt.config('watch', {
+    all: {
+      files: [
+        'Gruntfile.js',
+        '*.html'
+      ],
+      options: {
+        livereload: true
+      }
+    },
+    sass : {
+      files : ['scss/*.scss','scss/**/*.scss','scss/**/**/*.scss'],
+      tasks: ['sass:dev','postcss:dev']
+    },
+    js : {
+      files : ['scripts/*.js'],
+      tasks : ['concat:dev']
+    }
+  });
+
+  // Developer centric tasks
+  grunt.registerTask('dev', [], function(){
+    // for(i = 0; i < grunt.config('mods').dev.length ; i++){
+    //   grunt.loadNpmTasks(grunt.config('mods').dev[i]);
+    //   console.log(grunt.config('mods').dev[i]);
+    // }
+
+    grunt.task.run(
+      'sass:dev',
+      'postcss:dev',
+      'concat:dev',
+      'injector',
+      'express',
+      'open',
+      'watch'
+      );
+  });
+
+  // default task for developers
+  grunt.registerTask('default', ['dev']);
+
+  // build task for production stage
+  grunt.registerTask('build', [], function(){
+    grunt.task.run(
+      'sass:dist',
+      'concat:dist',
+      'uglify',
+      'htmlmin'
+      );
+  });
 };
