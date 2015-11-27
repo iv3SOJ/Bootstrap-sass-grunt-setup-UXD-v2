d5965dccee53481dd462e33870eac49b631ec0fc
jarvinm <mito.jan_arvin@yahoo.com>
2015-11-27 15:05:15 +0800

    grunt update

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,21 +57,6 @@ module.exports = function (grunt) {
 
     // Post process CSS files
     postcss : {
-      // options: {
-      //   // map: true, // inline sourcemaps
-
-      //   // or
-      //   map: {
-      //     inline: false, // save all sourcemaps as separate files...
-      //     annotation: '<%= config.app %>/css/maps/' // ...to the specified directory
-      //   },
-
-      //   processors: [
-      //     require('autoprefixer')({
-      //       browsers: 'last 2 versions'
-      //     }) // add vendor prefixes
-      // ]
-      // },
       dist: {
         src: '<%= config.dist %>/css/*.css',
         options : {
